cc858942b94fe0914542737a1b84c5ad6a31b38d
DongQS <dongqs@gmail.com>
2011-12-01 14:48:48 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elearning::Application.config.secret_token = '1376db4cd38b4b261b76c7c7c9254c49a0f4cd7c0f79d3bf2c56dcfb16f2121dedefc5ce627fcad67e198e93ce7f1654dea7bcd2297456b5420211a09c9cff0b'
